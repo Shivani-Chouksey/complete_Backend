582aeed2a22ec06ba2f670e69ad7558337dbe1ce
chouksey-shivani <shivani.chouksey@digitalsalt.in>
2025-04-22 15:29:30 +0530

    connect mongoDB DataBase

---
+++ 01_Chai_aur_code/03_SetUp_Professional_Backend/src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `\n MongoDB Connected !! DB Host : ${connectionInstance.connection.host} \n`
+    );
+  } catch (error) {
+    console.log("MONGO_DB connection FAILED:", error.message);
+    process.exit(1); // Exit the process with failure
+  }
+};
+
+export default connectDB;
