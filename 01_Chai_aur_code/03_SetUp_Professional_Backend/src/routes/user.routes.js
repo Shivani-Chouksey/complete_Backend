12f675f073111a1667c5ee6f552b963e057376e4
chouksey-shivani <shivani.chouksey@digitalsalt.in>
2025-05-02 10:37:37 +0530

    Add new Routes for user controller

--- 01_Chai_aur_code/03_SetUp_Professional_Backend/src/routes/user.routes.js
+++ 01_Chai_aur_code/03_SetUp_Professional_Backend/src/routes/user.routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import {
+  changeCurrentUserPassword,
+  currentUser,
+  getUserChannelProfile,
+  getUserWatchHistory,
   loginUser,
   logOutUser,
   refreshAccessToken,
   registerUser,
+  updateAccountDetails,
+  updateAvatar,
+  updateUserCoverImage,
 } from "../controllers/user.cotrollers.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -20,10 +27,27 @@ router.post(
 
 router.post("/login", loginUser);
 
-/* Secured Routes */
-router.post("/logout", verifyJWT, logOutUser);
-
 /* Refresh token */
 router.post("/refresh-token", refreshAccessToken);
+
+/* Secured Routes */
+router.post("/logout", verifyJWT, logOutUser);
+router.post("/change-password", verifyJWT, changeCurrentUserPassword);
+router.get("/current-user", verifyJWT, currentUser);
+router.patch("/update-account", verifyJWT, updateAccountDetails);
+router.patch(
+  "/update-avatar",
+  verifyJWT,
+  upload.single("avatar"),
+  updateAvatar
+);
+router.patch(
+  "/update-cover-image",
+  verifyJWT,
+  upload.single("/coverImage"),
+  updateUserCoverImage
+);
+router.get("/user-channel-profile/:username", verifyJWT, getUserChannelProfile);
+router.get("/watch-history", verifyJWT, getUserWatchHistory);
 
 export default router;
